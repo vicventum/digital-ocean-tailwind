19b859a1d49bd67c34a1f29f917058a31486dbd3
Víctor Álvarez <vicventum.job@gmail.com>
2024-06-11 19:43:49 -0400

    3.6 - Crea banner de estadísticas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import HeroSection from '@/components/sections/HeroSection.vue'
+import BannerStats from '@/components/banner/BannerStats.vue'
+
 </script>
 
 <template>
   <div>
     <HeroSection />
+    <BannerStats />
   </div>
 </template>
