a0cb7176ad9340c984a7c029f9a246f255f2b23f
Víctor Álvarez <vicventum.job@gmail.com>
2024-06-16 14:38:46 -0400

    Agrega RWD a la Banner Cta

--- src/components/banner/BannerCta.vue
+++ src/components/banner/BannerCta.vue
@@ -4,15 +4,15 @@ import BaseButton from '@/components/base/BaseButton.vue'
 
 <template>
   <section
-    class="bg-[url(@/assets/img/new-cta-band-bg-image.jpg)] bg-cover bg-center bg-scroll container flex items-center justify-between gap-x-8 rounded-lg bg-do-blue-dark px-16 py-24 text-white"
+    class="container flex flex-col items-center justify-between gap-8 rounded-lg bg-do-blue-dark bg-[url(@/assets/img/new-cta-band-bg-image.jpg)] bg-cover bg-scroll bg-center px-8 py-12 text-white lg:flex-row lg:px-16 lg:py-24"
   >
-    <div class="space-y-4 w-1/2">
+    <div class="lg:w-1/2 space-y-4 text-center lg:text-left">
       <h2 class="text-4xl font-bold">Start building today</h2>
-      <p class="text-lg font-medium text-pretty">
+      <p class="text-pretty text-lg font-medium">
         Sign up now and you'll be up and running on DigitalOcean in just minutes.
       </p>
     </div>
 
-    <BaseButton variant="cta" >Create your account</BaseButton>
+    <BaseButton variant="cta">Create your account</BaseButton>
   </section>
 </template>
