d5eb531e81555a42ae4797b8d544e05faff93d39
Víctor Álvarez <vicventum.dev@gmail.com>
2024-06-20 13:53:34 -0400

    Agrega data real a los artículos del blog

--- src/components/gallery/ArticlesGallery.vue
+++ src/components/gallery/ArticlesGallery.vue
@@ -5,56 +5,56 @@ const articles = [
     image: 'how-to-deploy-a-react-app-tutorial-image.jpg',
     type: 'tutorial',
     title: 'How To Deploy a React Application to DigitalOcean App Platform',
-    date: 'December 18, 2020',
+    date: 'January 18, 2024',
+    views: '319k'
+  },
+  {
+    image: 'how-to-install-mysql-tutorial-image.jpg',
+    type: 'tutorial',
+    title: 'How To Install MySQL on Ubuntu 24.04',
+    date: 'December 18, 2023',
     views: '22.8k'
   },
   {
-    image: 'how-to-deploy-a-react-app-tutorial-image.jpg',
-    type: 'tutorial',
-    title: 'How To Deploy a React Application to DigitalOcean App Platform',
-    date: 'December 18, 2020',
-    views: '22.8k'
+    image: 'how-to-scale-your-saas-product-blog-image.jpg',
+    type: 'blog',
+    title: 'How to scale your SaaS product without breaking the bank',
+    date: 'June 22, 2023',
+    // views: '22.8k'
   },
   {
-    image: 'how-to-deploy-a-react-app-tutorial-image.jpg',
+    image: 'how-to-install-nodejs-tutorial-image.jpg',
     type: 'tutorial',
-    title: 'How To Deploy a React Application to DigitalOcean App Platform',
-    date: 'December 18, 2020',
-    views: '22.8k'
+    title: 'How To Install Node.js on Ubuntu 20.04',
+    date: 'February 9, 2024',
+    views: '273.4k'
   },
   {
-    image: 'how-to-deploy-a-react-app-tutorial-image.jpg',
+    image: 'how-to-code-in-python-tutorial-image.jpg',
     type: 'tutorial',
-    title: 'How To Deploy a React Application to DigitalOcean App Platform',
-    date: 'December 18, 2020',
-    views: '22.8k'
+    title: 'How to code in Python',
+    date: 'August 23, 2022',
+    views: '16.1m'
   },
   {
-    image: 'how-to-deploy-a-react-app-tutorial-image.jpg',
-    type: 'tutorial',
-    title: 'How To Deploy a React Application to DigitalOcean App Platform',
-    date: 'December 18, 2020',
-    views: '22.8k'
+    image: 'contributing-to-open-source-blog-image.jpg',
+    type: 'blog',
+    title: 'Contributing to open source software: Kubernetes',
+    date: 'March 11, 2024',
+    // views: '22.8k'
   },
   {
-    image: 'how-to-deploy-a-react-app-tutorial-image.jpg',
-    type: 'tutorial',
-    title: 'How To Deploy a React Application to DigitalOcean App Platform',
-    date: 'December 18, 2020',
-    views: '22.8k'
+    image: 'introducing-digitalocean-managed-mongodb-blog-image.jpg',
+    type: 'blog',
+    title: 'Introducing Digital Ocean Managed MongoDB - a fully managed, database as service for modern apps',
+    date: 'July 31, 2023',
+    // views: '16.1m'
   },
   {
-    image: 'how-to-deploy-a-react-app-tutorial-image.jpg',
-    type: 'tutorial',
-    title: 'How To Deploy a React Application to DigitalOcean App Platform',
-    date: 'December 18, 2020',
-    views: '22.8k'
-  },
-  {
-    image: 'how-to-deploy-a-react-app-tutorial-image.jpg',
-    type: 'tutorial',
-    title: 'How To Deploy a React Application to DigitalOcean App Platform',
-    date: 'December 18, 2020',
+    image: 'multi-cloud-vs-hybrid-cloud-blog-image.jpg',
+    type: 'blog',
+    title: 'Multi-cloud vs hybrid cloud: Everything your business needs to know',
+    date: 'April 30, 2024',
     views: '22.8k'
   }
 ]
